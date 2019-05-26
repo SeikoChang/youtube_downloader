73ab40aaa4e7e5c51135baff2f726d9eb3c30d84
SeikoChang <seiko_chang@trendmicro.com>
2019-05-26 12:43:29 +0800

    version 1.0 ready

---
+++ youtube_downloader.py
@@ -0,0 +1,595 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#chcp 65001       #转换为utf-8代码页
+#chcp 936           #转换为默认的gb
+
+"""A simple command line application to download youtube videos."""
+from __future__ import absolute_import
+from __future__ import print_function
+
+import sys, os, tempfile, logging, argparse
+import argparse
+import time
+import datetime
+import gzip
+import json
+import io
+import subprocess
+import shutil
+import ntpath
+import re
+
+from pytube import __version__
+from pytube import YouTube
+
+PY3K = sys.version_info >= (3, 0)
+if PY3K:
+    import urllib.request as urllib2
+    import urllib.parse as urlparse
+else:
+    import urllib2 
+    import urlparse
+
+
+module = sys.modules['__main__'].__file__
+logger = logging.getLogger(module)
+
+
+def get_captions(url):
+    yt = YouTube(url)
+    captions = yt.captions.all()
+    if captions:
+        logger.info('captions = [%s] found' % captions)
+    else:
+        logger.debug('no any captions found!')
+    for caption in captions:
+        p = re.compile('<Caption lang=".*" code="(.*)">')
+        result = p.search(str(caption))
+        if result:
+            code = result.group(1)
+            logger.debug('captions code = [%s]' % code)
+            cap = caption.generate_srt_captions()
+            title = yt.title
+            stream = yt.streams.first()
+            filename = stream.default_filename
+            name, ext = os.path.splitext(filename)
+            fp = to_unicode('{}_{}.txt'.format(name, code))
+            with open(fp, 'wb') as fh:
+                fh.write(cap.encode('utf8'))
+
+
+def build_playback_report(url):
+    """Serialize the request data to json for offline debugging.
+    :param str url:
+        A valid YouTube watch URL.
+    """
+    yt = YouTube(url)
+    d = datetime.datetime.now()
+    ts = int(time.mktime(d.timetuple()))
+    fp = os.path.join(
+        os.getcwd(),
+        'yt-video-{yt.video_id}-{ts}.json.tar.gz'.format(yt=yt, ts=ts),
+    )
+
+    js = yt.js
+    watch_html = yt.watch_html
+    vid_info = yt.vid_info
+
+    with gzip.open(fp, 'wb') as fh:
+        fh.write(
+            json.dumps({
+                'url': url,
+                'js': js,
+                'watch_html': watch_html,
+                'video_info': vid_info,
+            })
+            .encode('utf8'),
+        )
+
+
+def display_streams(url):
+    """Probe YouTube video and lists its available formats.
+    :param str url:
+        A valid YouTube watch URL.
+    """
+    streams = []
+    yt = YouTube(url)
+    for stream in yt.streams.all():
+        streams.append(stream)
+        print(stream)
+    
+    return streams
+
+
+def get_target_itags(url, quality='NORMAL', mode='VIDEO_AUDIO'):
+    itags = [18]
+    yt = YouTube(url)
+    if mode and quality:
+        if mode.upper() == 'VIDEO_AUDIO':
+            streams = yt.streams.filter(progressive=True).order_by('itag').all()
+        elif mode.upper() == 'VIDEO':
+            streams= yt.streams.filter(only_video=True).order_by('itag').all()
+        elif mode.upper() == 'AUDIO':
+            streams = yt.streams.filter(only_audio=True).order_by('itag').all()
+        elif mode.upper() == 'ALL':
+            streams= yt.streams.all()
+        else:
+            return itags
+
+        if quality.upper() == 'HIGH':
+            streams = [streams[-1]]
+        elif quality.upper() == 'NORMAL':
+            streams = [median(streams)]
+        elif quality.upper() == 'LOW':
+            streams = [streams[0]]
+        elif quality.upper() == 'ALL':
+            pass
+        else:
+            return itags
+
+        if streams:
+            itags = []
+            for stream in streams:
+                print(stream)
+                p = re.compile('.*itag=\"(.*)\" mime_type=.*')
+                result = p.search(str(stream))
+                if result:
+                    itag = result.group(1)
+                    itags.append(itag)
+
+    return itags
+
+
+def get_terminal_size_windows():
+    try:
+        from ctypes import windll, create_string_buffer
+        import struct
+        # stdin handle is -10
+        # stdout handle is -11
+        # stderr handle is -12
+        h = windll.kernel32.GetStdHandle(-12)
+        csbi = create_string_buffer(22)
+        res = windll.kernel32.GetConsoleScreenBufferInfo(h, csbi)
+        if res:
+            (bufx, bufy, curx, cury, wattr,
+             left, top, right, bottom,
+             maxx, maxy) = struct.unpack("hhhhHhhhhhh", csbi.raw)
+            sizex = right - left + 1
+            sizey = bottom - top + 1
+            return sizex, sizey
+    except:
+        pass
+
+
+def get_terminal_size_stty():
+    try:
+        return map(int, subprocess.check_output(['stty', 'size']).split())
+    except:
+        pass
+
+
+def get_terminal_size_tput():
+    try:
+        return map(int, [subprocess.check_output(['tput', 'lines']), subprocess.check_output(['tput', 'rows'])])
+    except:
+        pass
+
+
+def get_terminal_size():
+    return get_terminal_size_windows() or get_terminal_size_stty() or get_terminal_size_tput() or (25, 80)
+
+
+def display_progress_bar(bytes_received, filesize, ch='█', scale=0.55): 
+    """Display a simple, pretty progress bar.
+    Example:
+    ~~~~~~~~
+    PSY - GANGNAM STYLE(강남스타일) MV.mp4
+    ↳ |███████████████████████████████████████| 100.0%
+    :param int bytes_received:
+        The delta between the total file size (bytes) and bytes already
+        written to disk.
+    :param int filesize:
+        File size of the media stream in bytes.
+    :param ch str:
+        Character to use for presenting progress segment.
+    :param float scale:
+        Scale multipler to reduce progress bar size.
+    """
+    _, columns = get_terminal_size()
+    max_width = int(columns * scale)
+
+    filled = int(round(max_width * bytes_received / float(filesize)))
+    remaining = max_width - filled
+    bar = ch * filled + ' ' * remaining
+    percent = round(100.0 * bytes_received / float(filesize), 1)
+    text = ' ↳ |{bar}| {percent}%\r'.format(bar=bar, percent=percent)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+
+def on_progress(stream, chunk, file_handle, bytes_remaining):
+    """On download progress callback function.
+    :param object stream:
+        An instance of :class:`Stream <Stream>` being downloaded.
+    :param file_handle:
+        The file handle where the media is being written to.
+    :type file_handle:
+        :py:class:`io.BufferedWriter`
+    :param int bytes_remaining:
+        How many bytes have been downloaded.
+    """
+    filesize = stream.filesize
+    bytes_received = filesize - bytes_remaining
+    display_progress_bar(bytes_received, filesize)
+
+
+def median(lst):
+    #sortedLst = sorted(lst)
+    lstLen = len(lst)
+    index = (lstLen - 1) // 2
+
+    if (lstLen % 2):
+        return lst[index]
+    else:
+        return lst[index]
+
+
+def filename_fix_existing(filename):
+    """Expands name portion of filename with numeric ' (x)' suffix to
+    return filename that doesn't exist already.
+    """
+    head, tail = ntpath.split(filename)
+    base = os.path.basename(filename)
+    name, ext = os.path.splitext(base)
+
+    if not head:
+        head = u'.'
+
+    try:
+        name, ext = tail.rsplit('.', 1)
+    except:
+        name = tail.rsplit(os.sep, 1)[0]
+        ext = None
+    names = [x for x in os.listdir(head) if x.startswith(name)]
+    if ext:
+        names = [x.rsplit('.', 1)[0] for x in names]
+    else:
+        names = [x.rsplit(os.sep, 1)[0] for x in names]
+    suffixes = [x.replace(name, '') for x in names]
+    # filter suffixes that match ' (x)' pattern
+    suffixes = [x[2:-1] for x in suffixes
+                   if x.startswith('_(') and x.endswith(')')]
+    indexes  = [int(x) for x in suffixes
+                   if set(x) <= set('0123456789')]
+    idx = 1
+    if indexes:
+        idx += sorted(indexes)[-1]
+
+    if ext:
+        out = '{0}_({1}).{2}'.format(name, idx, ext)
+    else:
+        out = '{0}_({1})'.format(name, idx)
+    out = os.path.join(head, out)
+    return out
+
+
+def to_unicode(filename):
+    """:return: filename decoded from utf-8 to unicode"""
+    if PY3K:
+        # [ ] test this on Python 3 + (Windows, Linux)
+        # [ ] port filename_from_headers once this works
+        # [ ] add test to repository / Travis
+        return filename
+    else:
+        if isinstance(filename, unicode): 
+            return filename
+        else:
+            return unicode(filename, 'utf-8')
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def loglevel_converter(loglevel):
+    numeric_level = getattr(logging, loglevel.upper(), logging.info)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    return numeric_level
+
+
+def get_arguments():
+    print(main.__doc__)
+
+    base = os.path.basename(__file__)
+    filename, file_extension = os.path.splitext(base)
+
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument('url', nargs='?', help=(
+        'The YouTube /watch url'
+        )
+    )
+    parser.add_argument(
+        '-v', '--version', action='version', version='%(prog)s ' + __version__, help=(
+            'Get current version of Pytube'
+        )
+    )
+    parser.add_argument(
+        '-t', '--itag', type=int, default=18, help=(
+            'The itag for the desired stream'
+        )
+    )
+    parser.add_argument(
+        '-l', '--list', action='store_true', help=(
+            'The list option causes pytube cli to return a list of streams available to download'
+        )
+    )
+    parser.add_argument(
+        '-bpr', '--build-playback-report', action='store_true', help=(
+            'Save the html and js to disk'
+        )
+    )
+
+    parser.add_argument(
+        "-rp", "--replace", type=str2bool, nargs='?', const=True, help=(
+            "idenfify if replace the exist file"
+        )
+    )
+    parser.add_argument(
+        "-o", "--out", action="store", type=str, help=(
+            "identify the destnation folder/filename to store the file"
+        )
+    )
+
+    parser.add_argument(
+        "-f", "--file", action="store", type=str, default='{name}.{ext}'.format(name=filename, ext='ini'), help=(
+            "identify the file which stored The YouTube /watch url(s)"
+        )
+    )
+    parser.add_argument(
+        "-r", "--retry",action="store", type=int, default=1, help=(
+            "retry time when get file failed"
+        )
+    )
+    parser.add_argument(
+        "-lf", "--logfile", action="store", type=str, default="{name}.{ext}".format(name=filename, ext='log'), help=(
+            "identify the log file name"
+        )
+    )
+    parser.add_argument(
+        "-ll", "--verbosity", type=str, default="DEBUG", choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'NOTSET'], help=(
+            "identify output verbosity"
+        )
+    )
+    parser.add_argument(
+        "-q", "--quiet", type=str2bool, nargs='?', const=True, help=(
+            "idenfify if enable the silent mode"
+        )
+    )
+    parser.add_argument(
+        "-x", "--proxy", action="store_true", help=(
+            "set proxy"
+        )
+    )
+    parser.add_argument(
+        "-qt", "--quality", type=str, choices=['HIGH', 'NORMAL', 'LOW', 'ALL'], help=(
+            "choose the quality of video to download"
+        )
+    )
+    parser.add_argument(
+        "-m", "--mode", type=str, choices=['VIDEO_AUDIO', 'VIDEO', 'AUDIO', 'ALL'], help=(
+            "choose only video/audio or video and audio together"
+        )
+    )
+    parser.add_argument(
+        "-cap", "--caption", action="store_true", help=(
+            "download all available cpation for all languages if available"
+        )
+    )
+
+    parser.set_defaults(replace=True)
+    parser.set_defaults(quiet=False)
+    args = parser.parse_args(sys.argv[1:])
+    print(args)
+    if not (args.url or os.path.exists(args.file)):
+        parser.print_help()
+
+    return args
+
+
+def download(url, itag=18, out=None, replace=True, proxies=None):
+    """Start downloading a YouTube video.
+    :param str url:
+        A valid YouTube watch URL.
+    :param str itag:
+        YouTube format identifier code.
+    """
+    # TODO(nficano): allow download target to be specified
+    # TODO(nficano): allow dash itags to be selected
+    yt = YouTube(url, on_progress_callback=on_progress, proxies=proxies)
+    stream = yt.streams.get_by_itag(itag)
+    filename = to_unicode(stream.default_filename)
+    filesize = stream.filesize
+    logger.info('\n{title} |\n{description} |\n\n{views} views | {rating} rating | {length} secs'.format(
+        title=yt.title,
+        description=yt.description,
+        views=yt.views,
+        rating=yt.rating,
+        length=yt.length
+    ))
+    print('\n{title} |\n{description} |\n\n{views} views | {rating} rating | {length} secs'.format(
+        title=yt.title,
+        description=yt.description,
+        views=yt.views,
+        rating=yt.rating,
+        length=yt.length
+    ))
+    print('\n{fn} | {fs} bytes'.format(
+        fn=filename,
+        fs=filesize
+    ))
+
+    # detect of out is a directory
+    outdir = None
+    if out:
+        if os.path.isdir(out):
+            outdir = out
+            out = None
+        else:
+            filename = out
+    if outdir:
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        filename = os.path.join(outdir, filename)
+    # add numeric ' (x)' suffix if filename already exists
+    if os.path.exists(filename) and not replace:
+        filename = filename_fix_existing(filename)
+    filename = to_unicode(filename)
+    name, ext = os.path.splitext(filename)
+    logger.info('target local filename = [%s]' % filename)
+    # create tmp file 
+    (fd, tmpfile) = tempfile.mkstemp(suffix=ext, prefix="", dir='.')
+    tmpfile = to_unicode(tmpfile)
+    os.close(fd)
+    os.unlink(tmpfile)
+    logger.info('target local tmpfile  = [%s]' % tmpfile)
+    tmppath, tmpbase = ntpath.split(tmpfile)
+    tmpname, tmpext = os.path.splitext(tmpbase)
+
+    try:
+        stream.download(output_path=None, filename=tmpname, filename_prefix=None)
+        sys.stdout.write('\n')
+        shutil.move(tmpfile, filename)
+        logger.info("File = [{0}] Saved".format(filename))
+    except KeyboardInterrupt:
+        sys.exit()
+
+    sys.stdout.write('\n')
+    return True
+
+
+def main():
+    """Command line application to download youtube videos."""
+    LogLevel = loglevel_converter(args.verbosity)
+    formatter = '%(asctime)s:[%(process)d]:[%(levelname)s]: %(message)s'
+
+    logger.handlers = []
+    logger.setLevel(LogLevel)
+
+    logging.basicConfig(
+                    level=LogLevel,
+                    format=formatter,
+                    datefmt='%a %b %d %H:%M:%S CST %Y'
+                    )
+
+    # new file handler
+    if args.logfile:
+        handler = logging.FileHandler(filename=args.logfile, mode='a+', encoding='utf-8', delay=True)
+        handler.setLevel(LogLevel)
+        # set logging format
+        formatter = logging.Formatter(formatter)
+        handler.setFormatter(formatter)
+        # add the handlers to the logger
+        logger.addHandler(handler)
+
+    if args.quiet:
+        logging.disable(logging.CRITICAL)
+
+    logger.debug('System out encoding = [%s]' % sys.stdout.encoding)
+
+    if not (args.url or os.path.exists(args.file)):
+        sys.exit(1)
+
+    base = os.path.basename(__file__)
+    filename, file_extension = os.path.splitext(base)
+
+    if args.proxy:
+        logger.info('via proxy = [%s]' % args.proxy)
+        proxy_params = {urlparse.urlparse(args.url).scheme: args.proxy}
+    else:
+        proxy_params = None
+
+    downloads = []
+    if args.url:
+        if args.list:
+            display_streams(args.url)
+
+        elif args.build_playback_report:
+            build_playback_report(args.url)
+
+        elif args.caption:
+            get_captions(args.url)
+
+        elif args.itag:
+            downloads.append(args.url)
+ 
+    elif args.file:
+        with open(args.file) as fp:
+            for line in fp:
+                downloads.append(line)
+
+    if len(downloads) > 0:
+        itags = [args.itag]
+        for url in downloads:
+            logger.info("trying to download url = {0}".format(url))
+            if args.quality and args.mode:
+                itags = get_target_itags(url=url, quality=args.quality, mode=args.mode)
+            for i in range(1, args.retry+1):
+                try:
+                    get_captions(url)
+                    replace = args.replace
+                    if len(itags) > 2:
+                        # change replace mode to always False if mutiple target found
+                        logger.debug('target number of files = [%s]' % len(itags))
+                        replace = False
+                    for i, itag in enumerate(itags):
+                        logger.debug('itag = [%s]' % itag)
+                        download(url=url, itag=itag, out=args.out, replace=replace, proxies=proxy_params)
+                    else:
+                        break
+                except:
+                    logger.exception('Unable to download Youtube from url = {0}'.format(url))
+
+    return True
+
+
+def unitest():
+    base = os.path.basename(__file__)
+    filename, file_extension = os.path.splitext(base)
+    url = 'https://www.youtube.com/watch?v=F1fqet9V494'
+
+    def test1():
+        args.list = True
+        main()
+    def test2():
+        args.build_playback_report = True
+        main()
+    def test3():
+        args.caption = True
+        main()
+    def test4():
+        main() 
+    def test5():
+        args.replace = True
+        args.quality = 'All'
+        args.mode = 'ALL'
+        main() 
+
+    args.url = url ; test1();test2();test3()
+    args.url = None ; args.file ='{name}.{ext}'.format(name=filename, ext='ini')
+    fp = to_unicode(args.file)
+    with open(fp, mode='wb') as fh:
+        fh.write(url)
+    test4();test5()
+
+
+if __name__ == "__main__":  # Only run if this file is called directly
+    args = get_arguments()
+    #unitest()
+    main()               
